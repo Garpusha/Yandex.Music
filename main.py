3ccf25b81b4f7baf5708abba2d270161a602bd2d
Garpusha <theflash@yandex.ru>
2023-03-13 22:59:35 +0300

    Albums

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 
-def get_albums(id):
+def get_albums(id_string):
 
     albums = []
-    url = f'https://music.yandex.ru/artist/{id}/albums'
+    url = f'https://music.yandex.ru/artist/{id_string}/albums'
     result = requests.get(url=url)
     if result.status_code != 200:
         print('Shit happens')
         exit()
     print('Let\'s go')
+    # Ищу название исполнителя
+    search_str = 'page-artist__title typo-h1 typo-h1_big">'
+    start_position = result.text.find(search_str)
+    end_position = result.text.find('<', start_position + len(search_str))
+    band = result.text[start_position+len(search_str):end_position]
     # Убираю лишнее
     start_position = result.text.find('Альбомы')
     end_position = result.text.find('Сборники', start_position)
@@ -16,42 +21,55 @@ def get_albums(id):
 
     # Считываю ссылку
     while True:
-        start_position = cutted_string.find('<a href="/album/')
+        search_str = '<a href="/album/'
+        start_position = cutted_string.find(search_str)
         if start_position == -1:
             break
-        start_position +=  + len('<a href="')
+        start_position += len(search_str)
         end_position = cutted_string.find('"', start_position)
-        album_link = 'https://music.yandex.ru' + cutted_string[start_position:end_position]
+        album_link = 'https://music.yandex.ru/album/' + cutted_string[start_position:end_position]
         cutted_string = cutted_string[end_position:]
 
     # Считываю название альбома
-        start_position = cutted_string.find('album__caption">')
+        search_str = 'album__caption">'
+        start_position = cutted_string.find(search_str)
         if start_position == -1:
             break
-        start_position += len('album__caption">')
+        start_position += len(search_str)
         end_position = cutted_string.find('<',start_position)
         album_name = cutted_string[start_position:end_position]
         cutted_string = cutted_string[end_position:]
 
         # Считываю название исполнителя
-        start_position = cutted_string.find('album__artist deco-typo-secondary typo-add" title="')
-        if start_position == -1:
-            break
-        start_position += len('album__artist deco-typo-secondary typo-add" title="')
-        end_position = cutted_string.find('"', start_position)
-        band_name = cutted_string[start_position:end_position]
-        cutted_string = cutted_string[end_position:]
+        # search_str = 'album__artist deco-typo-secondary typo-add" title="'
+        # start_position = cutted_string.find(search_str)
+        # if start_position == -1:
+        #     break
+        # start_position += len(search_str)
+        # end_position = cutted_string.find('"', start_position)
+        # band_name = cutted_string[start_position:end_position]
+        # cutted_string = cutted_string[end_position:]
 
     # Считываю год альбома
-        start_position = cutted_string.find('album__year deco-typo-secondary typo-add">')
+        search_str = 'album__year deco-typo-secondary typo-add">'
+        start_position = cutted_string.find(search_str)
         if start_position == -1:
             break
-        start_position += len('album__year deco-typo-secondary typo-add">')
+        start_position += len(search_str)
         end_position = cutted_string.find('<',start_position)
         album_year = cutted_string[start_position:end_position].strip()
         cutted_string = cutted_string[end_position:]
 
-        albums.append({'band':band_name, 'name':album_name, 'year':album_year, 'URL':album_link})
+        albums.append({'band':band, 'name':album_name, 'year':album_year, 'URL':album_link})
     return albums
 
-print(get_albums(10899))
+def write_to_file(my_list, filename):
+    with open(filename, 'w', encoding='utf32') as my_file:
+        for element in my_list:
+            for value in element.values():
+                my_string = f'{value};'
+                my_file.write(my_string)
+            my_file.write('\n')
+
+id = input('Enter musician ID (could be found at music.yandex.ru): ')
+write_to_file(get_albums(id),f'{id} albums.csv')
