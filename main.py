aa121dd8ae530135f50259bba4884ec119df4289
Garpusha <theflash@yandex.ru>
2023-03-16 16:05:48 +0300

    Re-building program structure_2

--- main.py
+++ main.py
@@ -16,16 +16,14 @@ def read_config(path, section, parameter):
     c_value = config.get(section, parameter)
     return c_value
 
-
-
-
-def set_to_str(my_set):
-    result_str = ''
-    for index, item in enumerate(my_set):
-        temp_str = ''
-        temp_str = f'{index + 1};{temp_str}{str(item)}\n'
-        result_str = result_str + temp_str
-    return result_str
+def parse_html(raw_text, start_seq, end_seq):
+    # search_str = '<a href="/album/'
+    start_position = raw_text.find(start_seq)
+    if start_position == -1:
+        return None
+    start_position += len(start_seq)
+    end_position = raw_text.find(end_seq, start_position)
+    return raw_text[start_position:end_position], end_position
 
 def parse_track(track_details):
     my_list = []
@@ -43,7 +41,27 @@ def parse_track(track_details):
 
     # my_list[1] = my_list[1][8:-1]
     # my_list[2] = my_list[2][7:-1]
-    return {'name':my_list[1], 'length':my_list[0], 'URL':my_list[2]}
+    return my_list[1], my_list[0], my_list[2]
+
+
+
+
+
+
+
+
+
+
+
+def set_to_str(my_set):
+    result_str = ''
+    for index, item in enumerate(my_set):
+        temp_str = ''
+        temp_str = f'{index + 1};{temp_str}{str(item)}\n'
+        result_str = result_str + temp_str
+    return result_str
+
+
 
 def get_tracks(album_url, min_tracks):
     tracks = []
@@ -208,15 +226,92 @@ def write_to_file(my_list):
 
 # start here ------------------------------------------------------------------------------
 # Reading config
-bands_list = read_config('config.ini', 'Main', 'BandID').split(',')
-collections = int(read_config('config.ini', 'Main', 'Collections'))
+
+band_list = read_config('config.ini', 'Main', 'BandID').split(',')
+collections_no = int(read_config('config.ini', 'Main', 'Collections'))
 collection_name = read_config('config.ini', 'Main', 'CollectionName')
 tracks_per_collection = int(read_config('config.ini', 'Main', 'TracksPerCollection'))
+min_tracks = int(read_config('config.ini', 'Main', 'SkipIfTracksLessThan'))
+
+# Structure:
+# genre = {'genre_id':, 'genre_name':}
+# band = {'band_id':, 'band_name':, 'band_url':}
+# album = {'album_id':, 'album_name':, 'album_year':, 'album_url':}
+# track = {'track_id':, 'track_name':, 'track_length':, 'track_url':, 'album_id':}
+# collection = {'collection_id':, 'collection_name':, 'collection_year':}
+# band_genre = {'band_genre_id':, 'band_id':, 'genre_id':}
+# band_album = {'band_album_id':, 'band_id':, 'album'_id':'}
+# collection_track = {'collection_track_id':, 'collection_id', 'track_id':}
+
+genre_id, band_id, album_id, track_id, collection_id = 0, 0, 0, 0, 0
+genres, bands, albums, tracks, collections = [], [], [], [], []
+band_genre_id, band_album_id, collection_track_id = 0, 0, 0
+band_genre, band_album, collection_track = [], [], []
+
+# Reading html pages:
+
+for band_id in band_list:
+    band_url = f'https://music.yandex.ru/artist/{band_id}/albums'
+    band_page = get_page(band_url)
+    band_page = band_page[band_page.find('Альбомы'): band_page.find('Сборники')]
+    band_page, cut_position  = parse_html(band_page, 'page-artist__title typo-h1 typo-h1_big">', '<')
+    band_page = band_page[cut_position:]
+    band_id += 1
+    bands.append({'id':band_id, 'band_name':band_name, 'band_url':band_url})
+    print(f'Musician found: {band_name}')
+
+    while True:
+        album_url, cut_position = parse_html(band_page, '<a href="/album/', '"')
+        if album_url == None:
+            break
+        album_url = 'https://music.yandex.ru/album/' + album_url
+        band_page = band_page[cut_position:]
+        album_name, cut_position = parse_html(band_page, 'album__caption">', '<')
+        band_page = band_page[cut_position:]
+        album_year, cut_position = parse_html(band_page, 'album__year deco-typo-secondary typo-add">', '<')
+        album_year = int(album_year)
+        band_page = band_page[cut_position:]
+
+        album_page = get_page(album_url)
+        tracks_num, cut_position = parse_html(album_page, '"numTracks":', ',')
+        tracks_num = int(tracks_num)
+        album_page = album_page[cut_position:]
+        if tracks_num < min_tracks:
+            pass
+        album_id += 1
+        albums.append({'id': album_id, 'album_name': album_name, 'album_year':album_year, 'album_url':album_url})
+        band_album_id += 1
+        band_album.append({'id':band_album_id, 'band_id':band_id, 'album_id':album_id})
+        print(f'Album found: {album_year} - {album_name}')
+
+        genre_name, cut_position = parse_html(album_page, '"genre":"', '"')
+        album_page = album_page[cut_position:]
+        is_genre = False
+        for genre in genres:
+            if genre_name == genre['genre_name']:
+                is_genre = True
+                break
+        if not is_genre:
+            genre_id += 1
+            genre.append({'id':genre_id, 'genre_name':genre_name})
+
+        for index in range(tracks_num):
+            track_string, cut_position = parse_html(album_page, '{"@type":"MusicRecording","duration":', '}')
+            album_page = album_page[cut_position:]
+            track_name, track_length, track_url = parse_track(track_string)
+            track_id += 1
+            tracks.append({'id':track_id, 'track_name':track_name, 'track_length':track_length, 'album_id':album_id})
+        print(f'Tracks found: {tracks_num}')
+    band_genre_id += 1
+    band_genre.append({'id':band_genre_id, 'band_id':band_id, 'genre_id':genre_id})
 
 
 
-for band_id in bands_list:
-    html_page = get_page(f'https://music.yandex.ru/artist/{band_id}/albums')
+
+
+
+
+
 
 discography = []
 
@@ -230,5 +325,6 @@ discography = []
 #         break
 for band_id in bands_list:
     discography.append(get_albums(band_id))
+
 write_to_file(discography)
 
